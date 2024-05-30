da365321e3cfe19c13c02c0f41a7eb1c59f4d8e6
Yana <yanassam81@gmail.com>
2024-05-30 20:51:18 +0200

    fix header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import ReduxTest from "./pages/ReduxTest/ReduxTest";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={null}></Route>
-      <Route path="/redux-test" element={<ReduxTest />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route index element={null}></Route>
+        <Route path="/redux-test" element={<ReduxTest />} />
+      </Routes>
+      <Header />
+    </>
   );
 }
 

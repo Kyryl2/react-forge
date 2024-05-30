8e37c6b89905961dccef66e01b0a9cdce546ac0d
Kyryl2 <krasiuk.4884@gmail.com>
2024-05-30 23:17:40 +0200

    create logic with routes+add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
 import { Route, Routes } from "react-router-dom";
 import ReduxTest from "./pages/ReduxTest/ReduxTest";
-// import RestrictedRoute from "./routes/RestrictedRoute";
-// import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
-// import LoginPage from "./pages/LoginPage/LoginPage";
+import RestrictedRoute from "./routes/RestrictedRoute";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { userRefreshThunk } from "./redux/auth/operations";
+import PrivateRoute from "./routes/PrivateRoute";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(userRefreshThunk());
+  }, [dispatch]);
   return (
-    <Routes>
-      {/* <Route
-        path="/register"
-        element={
-          <RestrictedRoute
-            redirectTo="/tasks"
-            component={<RegistrationPage />}
-          />
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
-        }
-      /> */}
-      <Route index element={null}></Route>
-      <Route path="/redux-test" element={<ReduxTest />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute redirectTo="/login" component={<DashboardPage />} />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/home"
+              component={<RegistrationPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/home" component={<LoginPage />} />
+          }
+        />
+
+        <Route path="/redux-test" element={<ReduxTest />} />
+      </Routes>
+    </>
   );
 }
 

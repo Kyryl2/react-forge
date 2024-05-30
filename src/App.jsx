9f01066424a60f25282c842a1cf108792566b487
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-05-30 17:23:38 +0300

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import ReduxTest from "./pages/ReduxTest/ReduxTest";
 
 function App() {
   return (
-    <>
-      <p style={{ fontFamily: "Popins" }}>Hello ,world!</p>
-      <Routes>
-        <Route index element={null}></Route>
-        <Route path="/redux-test" element={<ReduxTest />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route index element={null}></Route>
+      <Route path="/redux-test" element={<ReduxTest />} />
+    </Routes>
   );
-
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -16,6 +13,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </PersistGate>
     </Provider>
   </BrowserRouter>
-  // </React.StrictMode>
 );
-

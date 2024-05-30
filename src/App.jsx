27c0fb1e3d4cec57be89b595b0d2bd806334c7d4
Kyryl2 <krasiuk.4884@gmail.com>
2024-05-30 21:37:28 +0200

    add img svg and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import ReduxTest from "./pages/ReduxTest/ReduxTest";
+// import RestrictedRoute from "./routes/RestrictedRoute";
+// import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+// import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <Routes>
+      {/* <Route
+        path="/register"
+        element={
+          <RestrictedRoute
+            redirectTo="/tasks"
+            component={<RegistrationPage />}
+          />
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+        }
+      /> */}
       <Route index element={null}></Route>
       <Route path="/redux-test" element={<ReduxTest />} />
     </Routes>

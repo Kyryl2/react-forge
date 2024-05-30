149c178b8d2c91269c2ec89f4b9a7a923b6a0256
Kyryl2 <krasiuk.4884@gmail.com>
2024-05-30 13:26:02 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p style={{ fontFamily: "Popins" }}>Hello ,world!</p>;
+}
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,10 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
-      },
-      outDir: '../dist',
-    },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });

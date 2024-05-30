91b00ffb4790da05358f08e13b99f00c59e8d1a8
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-05-30 16:42:29 +0300

    Updated index, app, main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import ReduxTest from "./pages/ReduxTest/ReduxTest";
+
 function App() {
-  return <p style={{ fontFamily: "Popins" }}>Hello ,world!</p>;
+  return (
+    <>
+      <p style={{ fontFamily: "Popins" }}>Hello ,world!</p>
+      <Routes>
+        <Route index element={null}></Route>
+        <Route path="/redux-test" element={<ReduxTest />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>
+  // </React.StrictMode>
+);

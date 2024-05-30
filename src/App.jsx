a054da75df47bf2324263b1934ecfb78419ef575
YanaY27 <yanayeromina@gmail.com>
2024-05-30 19:36:22 +0300

    add form and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import ReduxTest from "./pages/ReduxTest/ReduxTest";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={null}></Route>
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/redux-test" element={<ReduxTest />} />
     </Routes>
   );
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;

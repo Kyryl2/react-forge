32f6e79151fd31cf01517fe2b5608ef7b75cd8c1
Yana <yanassam81@gmail.com>
2024-05-31 10:53:03 +0200

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import s from "./Header.module.css";
 import "../../styles/container.css";
+import "../../images/icons.svg";
+import favicon from "../../images/faviсon.png";
 
 const Header = () => {
   const handleExit = () => {
@@ -9,11 +11,17 @@ const Header = () => {
   return (
     <header className="container">
       <div className={s.header}>
-        <div className={s.logo}>Money Guard</div>
+        <div className={s.logo}>
+          <img src={favicon} alt="Money Guard Logo" className={s.logoImage} />
+          <div className={s.logoName}>Money Guard</div>
+        </div>
         <div className={s.userInfo}>
-          <span className={s.userName}>Name</span>
+          <div className={s.userName}>Name</div>
           <button className={s.exitButton} onClick={handleExit}>
-            <span className={s.exitIcon}>⬅</span>
+            {/* <span className={s.exitIcon}>⬅</span> */}
+            <svg className={s.exitIcon}>
+              <use href="#icon-home" />
+            </svg>
             <span className={s.exitText}>Exit</span>
           </button>
         </div>

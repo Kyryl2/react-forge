0a9449f755f1a6a9fb8fa80d3c689f3b59219ecd
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-05-31 12:39:26 +0300

    Redux test

--- src/pages/ReduxTest/ReduxTest.jsx
+++ src/pages/ReduxTest/ReduxTest.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import {
   userLoginThunk,
@@ -14,8 +14,12 @@ import {
   patchTransactionThunk,
   postTransactionThunk,
 } from "../../redux/transactions/operations.js";
+import { selectTransactions } from "../../redux/transactions/selectors.js";
 
 const ReduxTest = () => {
+  const transactions = useSelector(selectTransactions);
+  console.log(transactions);
+
   const dispatch = useDispatch();
 
   useEffect(() => {

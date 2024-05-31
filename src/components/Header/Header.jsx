2836b109b54ceb531fcb4e747d9c0810518f909c
Yana <yanassam81@gmail.com>
2024-05-31 14:59:18 +0200

    add userName in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import s from "./Header.module.css";
 import "../../styles/container.css";
-
-import favicon from "../../images/faviсon.png";
 import { useDispatch } from "react-redux";
 import { userLogoutThunk } from "../../redux/auth/operations";
 import { Icon } from "../../images/Icon/Icon";
@@ -10,7 +8,7 @@ import { useSelector } from "react-redux";
 
 const Header = () => {
   const dispatch = useDispatch();
-
+  const userName = useSelector((state) => state.auth.user.username);
   const handleExit = () => {
     dispatch(userLogoutThunk());
   };
@@ -22,12 +20,13 @@ const Header = () => {
           <div className={s.logoName}>Money Guard</div>
         </div>
         <div className={s.userInfo}>
-          <div className={s.userName}>Name</div>
+          {/* <div className={s.userName}>Name</div> */}
+          <div className={s.userName}>{userName || "Guest"}</div>
           <button className={s.exitButton} onClick={handleExit}>
             {/* <span className={s.exitIcon}>⬅</span> */}
             <svg className={s.exitIcon}>
               <use href="../../images/icons.svg#icon-home" />
-              <Icon id="icon-close" />
+              <Icon id="icon-exit" />
             </svg>
             <span className={s.exitText}>Exit</span>
           </button>

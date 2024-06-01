371272a1216cff2938787d607ac3df793784bc65
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-06-02 01:16:06 +0300

    added mobile table styling

--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -4,36 +4,51 @@ import { selectTransactions } from "../../redux/transactions/selectors";
 import { useEffect } from "react";
 import { getTransactionsThunk } from "../../redux/transactions/operations";
 import s from "./TransactionsList.module.css";
+import useMedia from "../../hooks/useMedia";
 
 const TransactionsList = () => {
   const transactions = useSelector(selectTransactions);
   const dispatch = useDispatch();
+  const { isMobile } = useMedia();
+
   useEffect(() => {
     dispatch(getTransactionsThunk());
   }, [dispatch]);
 
   return (
     <>
-      {!transactions && <p>You don t have any transactions now...</p>}
-      <div className={s.wrapper}>
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Type</th>
-              <th>Amount</th>
-              <th>Comment</th>
-              <th>Sum</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
+      {!transactions && <p>You don’t have any transactions now...</p>}
+
+      {!isMobile && (
+        <div className={s.wrapper}>
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Comment</th>
+                <th>Sum</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions?.map((transaction) => (
+                <TransactionsItem key={transaction.id} {...transaction} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {isMobile && (
+        <div className={s.wrapper}>
+          <ul>
             {transactions?.map((transaction) => (
               <TransactionsItem key={transaction.id} {...transaction} />
             ))}
-          </tbody>
-        </table>
-      </div>
+          </ul>
+        </div>
+      )}
     </>
   );
 };

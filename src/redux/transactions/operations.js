8abfda75a86807c5b0c454aa6b27dcb8d724f964
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-06-01 14:59:55 +0300

    Updated transaction summary operation

--- src/redux/transactions/operations.js
+++ src/redux/transactions/operations.js
@@ -20,13 +20,20 @@ export const getCategoriesThunk = createAsyncThunk(
 
 export const getSummaryThunk = createAsyncThunk(
   "transaction/getSummary",
-  async (_, thunkAPI) => {
+  async (query, thunkAPI) => {
     try {
+      const { month, year } = query;
+
       const { auth } = thunkAPI.getState();
 
       updateAuthHeader(auth.token);
 
-      const { data } = await goitApi.get("transactions-summary");
+      const { data } = await goitApi.get("transactions-summary", {
+        params: {
+          month,
+          year,
+        },
+      });
 
       return data;
     } catch (error) {

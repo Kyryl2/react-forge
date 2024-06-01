5b3b3f89c2bb1b9242caa9dedb3a1586394c837b
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-06-01 16:25:50 +0200

    css

--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -1,9 +1,9 @@
-
 import { useDispatch, useSelector } from 'react-redux';
 import TransactionsItem from '../TransactionsItem/TransactionsItem';
 import { selectTransactions } from '../../redux/transactions/selectors';
 import { useEffect } from 'react';
 import { getTransactionsThunk } from '../../redux/transactions/operations';
+import s from './TransactionsList.module.css';
 
 const TransactionsList = () => {
   const transactions = useSelector(selectTransactions);
@@ -14,27 +14,28 @@ const TransactionsList = () => {
 
   return (
     <>
-      {' '}
-      {!transactions && <p>You don`t have transaction now...</p>}
-      <table>
-        <thead>
-          <tr>
-            <th>Transaction Date</th>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Comment </th>
-            <th>Sum</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions?.map((transaction) => (
-            <TransactionsItem key={transaction.id} {...transaction} />
-          ))}
-        </tbody>
-      </table>
+      {!transactions && <p>You don t have any transactions now...</p>}
+      <div className={s.wrapper}>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Type</th>
+              <th>Amount</th>
+              <th>Comment</th>
+              <th>Sum</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions?.map((transaction) => (
+              <TransactionsItem key={transaction.id} {...transaction} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
-
 };
 
 export default TransactionsList;

7db2bf8219435085028754431beffcd0ddefd875
Yana <yanassam81@gmail.com>
2024-06-02 17:07:24 +0200

    fix btn

--- src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -1,3 +1,12 @@
+import { Icon } from "../../images/Icon/Icon";
+import s from "./ButtonAddTransactions.module.css";
+
 export const ButtonAddTransactions = () => {
-  return <div>ButtonAddTransactions</div>;
+  return (
+    // <div className={s.wrap}>
+    <button className={s.btn}>
+      <Icon id="icon-plus" width={20} height={20} className={s.icon} />
+    </button>
+    // </div>
+  );
 };

e3757611e6119b3129a4f720f011d326af4cbb80
Yana <yanassam81@gmail.com>
2024-06-02 22:21:30 +0200

    fix addTransForm

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -1,3 +1,4 @@
+import { Icon } from "../../images/Icon/Icon";
 import Modal from "../Modal/Modal";
 import Toggle from "../Toggle/Toggle";
 import s from "./AddTransactionForm.module.css";
@@ -6,13 +7,14 @@ export const AddTransactionForm = ({ closeModal }) => {
   return (
     <div>
       <Modal closeModal={closeModal}>
+        <Icon id="icon-close" width={16} height={16} className={s.iconClose} />
         <p className={s.title}>Add transaction</p>
         <Toggle />
         <div className={s.inputs}>
           <input type="number" placeholder="0.00" className={s.inputField} />
           <input type="date" className={s.dateInput} />
         </div>
-        <textarea placeholder="Comment" className={s.commentInput} />
+        <input placeholder="Comment" className={s.commentInput} />
 
         <button className={s.addButton}>ADD</button>
       </Modal>

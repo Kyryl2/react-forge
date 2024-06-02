8f7fdb6eb94f96e0022c27f38c83fc6511e530d3
Yana <yanassam81@gmail.com>
2024-06-02 20:36:01 +0200

    add addTransactionForm

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -1,3 +1,21 @@
-export const AddTransactionForm = () => {
-  return <div>AddTransactionForm</div>;
+import Modal from "../Modal/Modal";
+import Toggle from "../Toggle/Toggle";
+import s from "./AddTransactionForm.module.css";
+
+export const AddTransactionForm = ({ closeModal }) => {
+  return (
+    <div>
+      <Modal closeModal={closeModal}>
+        <p className={s.title}>Add transaction</p>
+        <Toggle />
+        <div className={s.inputs}>
+          <input type="number" placeholder="0.00" className={s.inputField} />
+          <input type="date" className={s.dateInput} />
+        </div>
+        <textarea placeholder="Comment" className={s.commentInput} />
+
+        <button className={s.addButton}>ADD</button>
+      </Modal>
+    </div>
+  );
 };
--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,15 +1,23 @@
+import React, { useState } from "react";
 import s from "./HomeTab.module.css";
 
 import TransactionsList from "../TransactionsList/TransactionsList";
 import { ButtonAddTransactions } from "../ButtonAddTransactions/ButtonAddTransactions";
+import { AddTransactionForm } from "../AddTransactionForm/AddTransactionForm";
 
 export default function HomeTab() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+    console.log("ddddd");
+  };
   return (
     <div className={s.tab}>
       <TransactionsList />
-      {/* <div className={s.container}> */}
-      <ButtonAddTransactions className={s.fixedButton} />
+
+      <ButtonAddTransactions onClick={toggleModal} className={s.fixedButton} />
+      {isModalOpen && <AddTransactionForm closeModal={toggleModal} />}
     </div>
-    // </div>
   );
 }

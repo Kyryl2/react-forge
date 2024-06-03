54ff1a4fcbcd002b1916899fb356501865d6cb69
Yana <yanassam81@gmail.com>
2024-06-03 13:13:47 +0200

    add Expense

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -1,9 +1,17 @@
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import { Icon } from "../../images/Icon/Icon";
 import Modal from "../Modal/Modal";
 import Toggle from "../Toggle/Toggle";
 import s from "./AddTransactionForm.module.css";
+import { useState } from "react";
 
 export const AddTransactionForm = ({ closeModal }) => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const [transactionType, setTransactionType] = useState(true);
+
   return (
     <div>
       <Modal closeModal={closeModal}>
@@ -15,10 +23,59 @@ export const AddTransactionForm = ({ closeModal }) => {
           onClick={closeModal}
         />
         <p className={s.title}>Add transaction</p>
-        <Toggle />
+        {/* <Toggle /> */}
+        <Toggle onChange={setTransactionType} />
+        {!transactionType && (
+          // <div>
+          <select className={s.categorySelect}>
+            <option value="">Select a category</option>
+
+            <option value="car">Car</option>
+            <option value="food">Food</option>
+            {/*и далее*/}
+          </select>
+          // </div>
+        )}
         <div className={s.inputs}>
           <input type="number" placeholder="0.00" className={s.inputField} />
-          <input type="date" className={s.dateInput} />
+          {/* <input type="date" className={s.dateInput} /> */}
+          <ReactDatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 48 48"
+              >
+                <mask id="ipSApplication0">
+                  <g
+                    fill="none"
+                    stroke="#fff"
+                    strokeLinejoin="round"
+                    strokeWidth="4"
+                  >
+                    <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
+                    <path
+                      fill="#fff"
+                      d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
+                    ></path>
+                  </g>
+                </mask>
+                <path
+                  fill="currentColor"
+                  d="M0 0h48v48H0z"
+                  mask="url(#ipSApplication0)"
+                ></path>
+              </svg>
+            }
+            dateFormat="dd.MM.yyyy"
+            className={s.dateInput}
+            // icon="fa fa-calendar"
+            // calendarClassName={s.datePickerCalendar}
+            // wrapperClassName={s.datePickerWrapper}
+          />
         </div>
         <input placeholder="Comment" className={s.commentInput} />
 

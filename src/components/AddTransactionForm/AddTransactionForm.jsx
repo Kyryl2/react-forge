3b7ccca833e232ce4746075752358b75f599b133
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-06-03 17:01:10 +0300

    added react select

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -1,25 +1,40 @@
+import { useEffect, useState } from "react";
 import ReactDatePicker from "react-datepicker";
+import Select from "react-select";
 import "react-datepicker/dist/react-datepicker.css";
-
 import { Icon } from "../../images/Icon/Icon";
 import Modal from "../Modal/Modal";
 import Toggle from "../Toggle/Toggle";
 import s from "./AddTransactionForm.module.css";
-import { useEffect, useState } from "react";
-import { selectCategories } from "../../redux/transactions/selectors";
 import { useDispatch, useSelector } from "react-redux";
+import { selectCategories } from "../../redux/transactions/selectors";
 import { getCategoriesThunk } from "../../redux/transactions/operations";
-
+import { styles } from "../../options/selectStylesAdd";
+import clsx from "clsx";
 export const AddTransactionForm = ({ closeModal }) => {
+  const [monthSelectIsOpen, setMonthSelectIsOpen] = useState(false);
   const [startDate, setStartDate] = useState(new Date());
   const [transactionType, setTransactionType] = useState(true);
 
   const dispatch = useDispatch();
   const categories = useSelector(selectCategories);
 
+  const handleMenuOpen = (id) => {
+    if (id === "monthSelect") setMonthSelectIsOpen(true);
+  };
+
+  const handleMenuClose = (id) => {
+    if (id === "monthSelect") setMonthSelectIsOpen(false);
+  };
+
   useEffect(() => {
     dispatch(getCategoriesThunk());
   }, [dispatch]);
+
+  const categoryOptions = categories.map((category) => ({
+    value: category.id,
+    label: category.name,
+  }));
 
   return (
     <div>
@@ -32,60 +47,37 @@ export const AddTransactionForm = ({ closeModal }) => {
           onClick={closeModal}
         />
         <p className={s.title}>Add transaction</p>
-        {/* <Toggle /> */}
         <Toggle onChange={setTransactionType} />
         {!transactionType && (
-          <select className={s.categorySelect}>
-            {categories.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </select>
+          <div className={s.select_wrapper}>
+            <Select
+              className={s.categorySelect}
+              options={categoryOptions}
+              placeholder="Select category"
+              onMenuOpen={() => handleMenuOpen("monthSelect")}
+              onMenuClose={() => handleMenuClose("monthSelect")}
+              styles={styles}
+            />
+            <Icon
+              id="icon-down-arrow"
+              className={clsx(s.icon, {
+                [s.is_active]: monthSelectIsOpen,
+              })}
+              width="20px"
+              height="11px"
+            />
+          </div>
         )}
         <div className={s.inputs}>
           <input type="number" placeholder="0.00" className={s.inputField} />
-          {/* <input type="date" className={s.dateInput} /> */}
           <ReactDatePicker
             selected={startDate}
             onChange={(date) => setStartDate(date)}
-            icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                viewBox="0 0 48 48"
-              >
-                <mask id="ipSApplication0">
-                  <g
-                    fill="none"
-                    stroke="#fff"
-                    strokeLinejoin="round"
-                    strokeWidth="4"
-                  >
-                    <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
-                    <path
-                      fill="#fff"
-                      d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
-                    ></path>
-                  </g>
-                </mask>
-                <path
-                  fill="currentColor"
-                  d="M0 0h48v48H0z"
-                  mask="url(#ipSApplication0)"
-                ></path>
-              </svg>
-            }
             dateFormat="dd.MM.yyyy"
             className={s.dateInput}
-            // icon="fa fa-calendar"
-            // calendarClassName={s.datePickerCalendar}
-            // wrapperClassName={s.datePickerWrapper}
           />
         </div>
         <input placeholder="Comment" className={s.commentInput} />
-
         <button className={s.addButton}>ADD</button>
       </Modal>
     </div>

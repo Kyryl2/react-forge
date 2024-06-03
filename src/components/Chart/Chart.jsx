c75e9a7b7cd90fb0d3fc7e8622f39375b3c95670
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-06-03 22:03:16 +0300

    Chart refactored

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,89 +1,34 @@
-import css from "./Chart.module.css";
 import { Doughnut } from "react-chartjs-2";
+import { useSelector } from "react-redux";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { useDispatch, useSelector } from "react-redux";
+import css from "./Chart.module.css";
+import { getData, getOptions } from "../../helpers/chartOptions";
 import { selectSummary } from "../../redux/transactions/selectors";
-import { getCategoriesThunk } from "../../redux/transactions/operations";
-import { useEffect } from "react";
+import { getCategoryColor } from "../../helpers/getCategoryColor";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const Chart = () => {
   const { categoriesSummary, periodTotal } = useSelector(selectSummary);
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getCategoriesThunk());
-  }, [dispatch]);
+  const categoriesTotal = categoriesSummary?.map((category) => {
+    return category.total;
+  });
+  const categoryColors = categoriesSummary?.map((category) => {
+    return getCategoryColor(category.name);
+  });
 
-  const getCategoryColor = (categoryName) => {
-    switch (categoryName) {
-      case "Main expenses":
-        return "#fed057";
-      case "Products":
-        return "#ffd8d0";
-      case "Car":
-        return "#fd9498";
-      case "Self care":
-        return "#c5baff";
-      case "Child care":
-        return "#6e78e8";
-      case "Household products":
-        return "#4a56e2";
-      case "Education":
-        return "#81e1ff";
-      case "Leisure":
-        return "#24cca7";
-      case "Other expenses":
-        return "#00ad84";
-      case "Entertainment":
-        return "#ffbf00";
-      case "Income":
-        return "#00c853";
-      default:
-        return "#ffffff";
-    }
-  };
-
-  const categoriesTotal = categoriesSummary?.map((category) => category.total);
-  const categoryColors = categoriesSummary?.map((category) =>
-    getCategoryColor(category.name)
-  );
-
-  const data = {
-    datasets: [
-      {
-        data: categoriesTotal,
-        backgroundColor: categoryColors,
-        borderColor: "transparent",
-        cutout: "70%",
-      },
-    ],
-  };
-
-  const options = {
-    plugins: {
-      tooltip: {
-        callbacks: {
-          label: (context) => {
-            const category = categoriesSummary[context.dataIndex].name;
-            const value = context.raw;
-            return `${category}: ${value}`;
-          },
-        },
-        displayColors: false,
-        backgroundColor: "",
-        borderColor: "rgba(0, 0, 0, 0)",
-        borderRadius: 5,
-        padding: 10,
-      },
-    },
-  };
+  const data = getData(categoriesTotal, categoryColors);
+  const options = getOptions(categoriesSummary);
 
   return (
     <div className={css.wrapper}>
       <Doughnut data={data} options={options} />
-      <span className={css.balance}>&#8372; {periodTotal}</span>
+      <span className={css.balance}>
+        {categoriesSummary?.length > 0
+          ? `₴ ${periodTotal} `
+          : "No transactions"}
+      </span>
     </div>
   );
 };

1a782eeed585b26c777f5ea81c15310d6ce7fe52
Kyryl2 <krasiuk.4884@gmail.com>
2024-06-04 20:09:27 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import RestrictedRoute from "./routes/RestrictedRoute";
 import PrivateRoute from "./routes/PrivateRoute";
-import ReduxTest from "./pages/ReduxTest/ReduxTest";
-import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import DashboardPage from "./pages/DashboardPage/DashboardPage";
-import HomeTab from "./components/HomeTab/HomeTab";
-import StatisticsTab from "./components/StatisticsTab/StatisticsTab";
 import Loader from "./components/Loader/Loader";
-import Currency from "./components/Currency/Currency";
-import NotFound from "./pages/NotFound/NotFound";
 
 import { userRefreshThunk } from "./redux/auth/operations";
 import { selectIsRefreshing } from "./redux/auth/selectors";
 import useMedia from "./hooks/useMedia";
+import { easyLazyPage, easyLazyComponent } from "./helpers/easyLazy";
+
+const NotFound = easyLazyPage("NotFound");
+const DashboardPage = easyLazyPage("DashboardPage");
+const LoginPage = easyLazyPage("LoginPage");
+const RegistrationPage = easyLazyPage("RegistrationPage");
+const HomeTab = easyLazyComponent("HomeTab");
+const StatisticsTab = easyLazyComponent("StatisticsTab");
+const Currency = easyLazyComponent("Currency");
 
 function App() {
   const isRefreshing = useSelector(selectIsRefreshing);
@@ -31,34 +32,40 @@ function App() {
       {isRefreshing ? (
         <Loader />
       ) : (
-        <Routes>
-          <Route
-            path="/"
-            element={<PrivateRoute component={<DashboardPage />} />}
-          >
-            <Route index element={<HomeTab />} />
-            <Route path="dashboard" element={<DashboardPage />} />
-
-            <Route path="statistics" element={<StatisticsTab />} />
+        <Suspense
+          fallback={
+            <div>
+              <Loader />
+            </div>
+          }
+        >
+          <Routes>
             <Route
-              path="currency"
-              element={isMobile ? <Currency /> : <Navigate to="/" />}
+              path="/"
+              element={<PrivateRoute component={<DashboardPage />} />}
+            >
+              <Route index element={<HomeTab />} />
+
+              <Route path="statistics" element={<StatisticsTab />} />
+              <Route
+                path="currency"
+                element={isMobile ? <Currency /> : <Navigate to="/" />}
+              />
+            </Route>
+
+            <Route
+              path="/register"
+              element={<RestrictedRoute component={<RegistrationPage />} />}
             />
-          </Route>
 
-          <Route
-            path="/register"
-            element={<RestrictedRoute component={<RegistrationPage />} />}
-          />
+            <Route
+              path="/login"
+              element={<RestrictedRoute component={<LoginPage />} />}
+            />
 
-          <Route
-            path="/login"
-            element={<RestrictedRoute component={<LoginPage />} />}
-          />
-
-          <Route path="/redux-test" element={<ReduxTest />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       )}
     </>
   );
